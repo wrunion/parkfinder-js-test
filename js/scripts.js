678a2f68de22e069bc2f7bb79db03848142f44e4
Winter Runion <winterrunion@gmail.com>
2019-07-15 13:13:17 -0700

    Create sample template form.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*How to get values from textboxes, dates, and drop downs:*/
+var beverage = $("#beverage").val();
+
+/*How to get values from radio buttons:*/
+var flavor = $("input:radio[name=flavor]:checked").val();
+
+
+/*How to get values from checkboxes:*/
+var flavor = $("input:checkbox[name=flavor]:checked").val();
