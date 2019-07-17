b25656575ee3141d57a2384a1a5b37285151a6be
Winter Runion <winterrunion@gmail.com>
2019-07-16 18:09:29 -0700

    Add a notes file, and a scripts experiment (note: I edited the html for the purpose of this experiment, not to change it forever. Just to see if this function would work.

---
+++ NOTES.md
@@ -0,0 +1,20 @@
+### Just a list of notes and resources so I can remember them.
+
+Websites:
+
+For free vector graphics (check usage requirements before using; most require credit but are free to use):
+https://www.freepik.com/search?dates=any&format=search&page=2&query=rainbow&selection=1&sort=popular&type=vector
+
+Free graphics & photos www.pixabay.com
+
+Ideas for how to do the javascript database
+https://stackoverflow.com/questions/13610731/creating-a-small-searchable-database-with-javascript-object-arrays-only-no-ajax
+
+Related jfiddle:
+http://jsfiddle.net/H9jkd/
+
+building a database with PHP and mySQL
+https://www.youtube.com/watch?v=yAmXgDKu2go
+
+##### How to display text when a checkbox is checked:
+* https://www.w3schools.com/howto/howto_js_display_checkbox_text.asp
--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,37 @@ var flavor = $("input:radio[name=flavor]:checked").val();
 
 /*How to get values from checkboxes:*/
 var flavor = $("input:checkbox[name=flavor]:checked").val();
+
+
+/*Sample script we may want to copy from*/
+// <script type="text/javascript">
+// function doSearch(roomquery) {
+// alert('roomquery is ' + roomquery);
+// }
+// </script>
+//
+// how many rooms? <select onchange="doSearch(this.options[selectedIndex].text);">
+//     <option>1</option>
+//     <option>2</option>
+// </select>
+//Source: http: jsfiddle.net/H9jkd/
+
+//Business Logic
+//Creating the "Park" object constructor function. Reference: https://www.learnhowtoprogram.com/intro-to-programming-evening/object-oriented-javascript-3cb65911-fc7b-4e3d-ae84-8af3cbed4b34/constructors-and-prototypes
+function Park(playground, parking, garden, restroom, accessibility, zone, pool) {
+  this.playground = playground;
+  this.parking = parking;
+  this.garden = garden;
+  this.restroom = restroom;
+  this.accessibility = accessibility;
+  this.zone = zone;
+  this.pool = pool;
+}
+
+var rainbowPark = new Park(true, false, true, true, false, true, true);
+var unicornPark = new Park(true, true, false, true, true, false, true);
+var dragonPark = new Park(false, true, true, true, false, true, true);
+var hobbitPark = new Park(true, false, true, true, true, false, false);
+var galaxyPark = new Park(false, true, true, true, false, true, false);
+
+//UI Logic
